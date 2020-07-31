92cebc7dc28994b1d3274d1fa89a7cfb8f4ccdc5
Vitor Santos <vitor.santos430@etec.sp.gov.br>
2020-07-31 19:53:04 -0300

    Cliente com C#

---
+++ consumindoAPI_Csharp/consumindoAPI/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace consumindoAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define URL 
+            HttpClient cliente = new HttpClient();
+            cliente.BaseAddress = new Uri("http://localhost/");
+
+            // Define cabeçalho HTTP para receber JSON
+            cliente.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // Aguarda Resposta da requisição
+            HttpResponseMessage response = cliente.GetAsync("api/v1/estoque/mostrar").Result;
+
+            // Mostra Cabeçalho da requisição
+            Console.WriteLine(response);
+
+            if (response.IsSuccessStatusCode)
+            {
+                
+                // Passa resposta do corpo
+                var dataObjects = response.Content.ReadAsStringAsync().Result; 
+
+                dynamic resultado = Newtonsoft.Json.JsonConvert.DeserializeObject(dataObjects);
+
+                Console.WriteLine("  **  Produtos filial  ** \n");
+
+                Console.WriteLine("ID\t\tProduto\t\tPreço\n");
+
+                foreach(var dado in resultado.dados)
+                {
+                    Console.WriteLine("{0}\t \t{1}\t {2}",dado.id, dado.produto, dado.preco);
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            } 
+
+            Console.ReadKey();
+        }
+    }
+}
